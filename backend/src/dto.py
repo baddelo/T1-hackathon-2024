80e068ac43ad9af073962c9ba8c2436f941703e1
Zlyden-1 <Zlyden-1@yandex.ru>
2024-10-26 02:22:04 +0400

    :beers: add default values to dto

--- backend/src/dto.py
+++ backend/src/dto.py
@@ -7,13 +7,14 @@ from pydantic import BaseModel, ConfigDict
 class LanguageEnum(str, Enum):
     RUSSIAN = 'rus'
     ENGLISH = 'eng'
+    UNKNOWN = ''
 
 
 class OutputDTO(BaseModel):
     coordinates: Tuple[Tuple[int, int], Tuple[int, int]]
-    content: str
-    lang: LanguageEnum
-    signature: bool
+    content: str = ''
+    lang: LanguageEnum = LanguageEnum.UNKNOWN
+    signature: bool = False
 
     model_config = ConfigDict(
         use_enum_values=True,
