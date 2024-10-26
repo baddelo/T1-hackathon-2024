86c1b98f630f969e542480f81cf75351d3d13412
Zlyden-1 <Zlyden-1@yandex.ru>
2024-10-26 23:41:23 +0400

    fix new model

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,21 @@
 from typing import List, Dict, Any
 
 import numpy as np
+import torch
 from PIL.Image import Image
 import bentoml
 
 from src.dto import OutputDTO
+from src.model.ocr_transformer import (
+    TransformerModel,
+    ALPHABET,
+    HIDDEN,
+    ENC_LAYERS,
+    DEC_LAYERS,
+    N_HEADS,
+    DROPOUT,
+    DEVICE
+)
 
 
 @bentoml.service(
@@ -14,7 +25,16 @@ from src.dto import OutputDTO
 class TextDetector:
     def __init__(self) -> None:
         from ultralytics import YOLO
-        self.model = YOLO('trained_models/yolov8_weights_v2.pt')
+        self.model_detection = YOLO('trained_models/yolov8_weights_v2.pt')
+        self.model_recognition = TransformerModel(
+            len(ALPHABET),
+            hidden=HIDDEN,
+            enc_layers=ENC_LAYERS,
+            dec_layers=DEC_LAYERS,
+            nhead=N_HEADS,
+            dropout=DROPOUT
+        ).to(DEVICE)
+        # self.model_recognition.load_state_dict(torch.load(''))
 
     def convert_output_to_dto(self, predictions) -> List[OutputDTO]:
         output_dtos = []
@@ -31,8 +51,7 @@ class TextDetector:
             output_dtos.append(output_dto)
         return output_dtos
 
-
     @bentoml.api
     def detect(self, image: Image) -> List[OutputDTO]:
-        result = self.model(image)
+        result = self.model_detection(image)
         return self.convert_output_to_dto(result)
