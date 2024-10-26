8e38b1f9ed1abd4282242cf8b9d1f0209b2f3ab8
Zlyden-1 <Zlyden-1@yandex.ru>
2024-10-26 04:07:13 +0400

    :beers: add default model

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,15 +12,19 @@ from src.dto import OutputDTO
 )
 class TextDetector:
     def __init__(self) -> None:
-        self.model = self.get_model(2)
+        if not bentoml.models.list():
+            self.get_model(2)
+        self.model = bentoml.pytorch.load_model("test_detection")
+        # lambda x: [OutputDTO(coordinates=((0, 0), (0, 0)), content="xui", lang='eng', signature=False)]
 
     def get_model(self, num_classes):
+        import torchvision
+        from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
+        bentoml.pytorch.save_model("test_detection", model)
         return model
-
 
     @bentoml.api
     def detect(self, image: Image) -> List[OutputDTO]:
